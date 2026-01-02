6605144b88378e0486a4e216064d82de72d8d5e8
pb-96 <philip.byrne@faculty.ai>
2026-01-02 13:26:52 +0000

    adding main server space

---
+++ agent-notifications/server/main.ts
@@ -0,0 +1,12 @@
+// import * as vscode from 'vscode';
+
+
+const main_loop = async () => {
+    while (true) {
+        // Main logic await here
+        await new Promise(resolve => setTimeout(resolve, 1000));
+    }
+};
+
+
+
